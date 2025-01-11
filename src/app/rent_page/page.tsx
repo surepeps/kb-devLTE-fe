bb00616cf5cd9ddc39d9e56161db8d473ea86f77
Oluwafemi <oluwafemiomolounnu@gmail.com>
2025-01-11 07:47:27 +0100

    screen: created the rent page

---
+++ src/app/rent_page/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Rent(){
+    return(
+        <div className="w-full h-screen flex justify-center items-center">
+            <h1> Rent Page </h1>
+        </div>
+    )
+};
