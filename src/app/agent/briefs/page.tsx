59e88e23a3a0c57332a46594fb1077390654e17c
Abasay <abdulsalamasheem@gmail.com>
2025-03-01 06:40:22 +0100

    fixed
    Former-commit-id: b1abcf0d30862147f50165dd3981800ddc23c176

--- src/app/agent/briefs/page.tsx
+++ src/app/agent/briefs/page.tsx
@@ -9,34 +9,33 @@ import PropertyType from '@/components/propertyType';
 import Overview from '@/components/overview';
 import { briefData, completeTransactionData } from '@/data/sampleDataForAgent';
 import Brief from '@/components/brief';
+// import { useUserContext } from '@/context/user-context';
 
 const Form2 = () => {
   const { isContactUsClicked, isModalOpened } = usePageContext();
   const { selectedNav } = usePageContext();
+  // const { user } = useUserContext();
 
   /**TotalBrief */
   const [showFullDetails, setShowFullDetails] = useState<boolean>(false);
   const [detailsToCheck, setDetailsToCheck] = useState<DataProps>(briefData[0]);
 
   /**Draft Brief */
-  const [showDraftBriefFullDetails, setShowDraftBriefFullDetails] =
-    useState<boolean>(false);
-  const [detailsToCheckForDraftBrief, setDetailsToCheckForDraftBrief] =
-    useState<DataProps>(briefData[0]);
+  const [showDraftBriefFullDetails, setShowDraftBriefFullDetails] = useState<boolean>(false);
+  const [detailsToCheckForDraftBrief, setDetailsToCheckForDraftBrief] = useState<DataProps>(briefData[0]);
 
   /**Transaction History */
-  const [showTransactionHistory, setShowTransactionHistory] =
-    useState<boolean>(false);
-  const [
-    detailsToCheckForTransactionHistory,
-    setDetailsToCheckForTransactionHistory,
-  ] = useState<DataProps>(briefData[0]);
+  const [showTransactionHistory, setShowTransactionHistory] = useState<boolean>(false);
+  const [detailsToCheckForTransactionHistory, setDetailsToCheckForTransactionHistory] = useState<DataProps>(
+    briefData[0]
+  );
 
   return (
     <section
       className={`flex w-full items-center filter justify-center transition duration-500 bg-[#EEF1F1] min-h-[800px] py-[40px]  ${
         (isContactUsClicked || isModalOpened) && 'brightness-[30%]'
-      }`}>
+      }`}
+    >
       <div className='container flex flex-col min-h-[700px] items-center p-[20px]'>
         <AgentNav />
         {selectedNav === AgentNavData.OVERVIEW && <Overview />}
@@ -79,14 +78,7 @@ const Form2 = () => {
   );
 };
 
-const headerData: string[] = [
-  'Date',
-  'Property Type',
-  'Location',
-  'Property price',
-  'Document',
-  'Full details',
-];
+const headerData: string[] = ['Date', 'Property Type', 'Location', 'Property price', 'Document', 'Full details'];
 
 const transactionHeaderData: string[] = [
   'Date',
