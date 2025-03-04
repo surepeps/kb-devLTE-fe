129eb0f602988f4368d5bf999aa1ec08bd216853
Oluwafemi Omolounnu <oluwafemiomolounnu@gmail.com>
2025-03-04 23:42:27 +0100

    fixed policy page

--- src/components/home_helpbutton.tsx
+++ src/components/home_helpbutton.tsx
@@ -13,13 +13,17 @@ const HelpButton = () => {
         <div className='flex justify-center items-center md:flex-row flex-col gap-[28px] flex-wrap'>
           <Button
             type='button'
-            onClick={() => {}}
+            onClick={() => {
+              window.location.href = '/buy_page'
+            }}
             value={`Submit Your Preference`}
             className='h-[50px] font-bold text-[#FFFFFF] text-base min-w-[249px] py-[12px] px-[24px] bg-[#8DDB90] leading-[25.6px]'
           />
           <Button
             type='button'
-            onClick={() => {}}
+            onClick={() => {
+              window.location.href = '/sell_page'
+            }}
             value={`Submit Your Brief`}
             className='h-[50px] font-bold text-[#FFFFFF] text-base min-w-[249px] py-[12px] px-[24px] bg-[#8DDB90] leading-[25.6px]'
           />
