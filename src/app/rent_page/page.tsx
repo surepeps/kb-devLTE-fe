493022ff7e941e4fd5a66d8f4644410b540a303b
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2025-03-05 00:05:12 +0100

    `Integrating APIs in the buy page`
    Former-commit-id: 7a7b5642df1eab7f9f7f17ad85445408062a4aa2

--- src/app/rent_page/page.tsx
+++ src/app/rent_page/page.tsx
@@ -1,39 +1,60 @@
 /** @format */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 import Loading from '@/components/loading';
 import { useLoading } from '@/hooks/useLoading';
 import { propertyReferenceDataWithoutUsageOption } from '@/data/buy_page_data';
 import { usePageContext } from '@/context/page-context';
 //import Card from '@/components/card';
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 //import Button from '@/components/button';
 import ContactUs from '@/components/contact_information';
 import RentalReference from '@/components/rentalReference';
 import HouseFrame from '@/components/house-frame';
-import imgSample from '@/assets/assets.png';
+//import imgSample from '@/assets/assets.png';
 import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import { URLS } from '@/utils/URLS';
 
+type HouseFrameProps = {
+  propertyType: string;
+  pictures: string[];
+  features: { featureName: string; id: string }[];
+  location: {
+    state: string;
+    area: string;
+    localGovernment: string;
+  };
+  noOfBedrooms: number;
+  _id: string;
+};
 export default function Rent() {
   const isLoading = useLoading();
   const { rentPage } = usePageContext();
   const router = useRouter();
-  // const [found, setFound] = useState({
-  //   isFound: false,
-  //   count: 0,
-  // });
-  //const [isSelectedBriefClicked, setIsSelectedBriefClicked] =
-  useState<boolean>(false);
-  //const [text, setText] = useState<string>('View selected Brief');
+  const [isDataLoading, setDataLoading] = useState<boolean>(false);
+  const [data, setData] = useState<any[]>([]);
 
-  // const viewSelectedBrief = () => {
-  //   if (text === 'View selected Brief') {
-  //     setIsSelectedBriefClicked(true);
-  //     setText('Hide selected Brief');
-  //   } else if (text === 'Hide selected Brief') {
-  //     setIsSelectedBriefClicked(!isSelectedBriefClicked);
-  //     setText('View selected Brief');
-  //   }
-  // };
+  useEffect(() => {
+    const getAllRentProperties = async () => {
+      setDataLoading(true);
+      try {
+        const resposne = await axios.get(URLS.BASE + '/properties/rents/all');
+        console.log(resposne);
+        if (resposne.status === 200) {
+          setData(resposne.data.slice(0, 8));
+          setDataLoading(false);
+        }
+      } catch (error) {
+        console.log(error);
+        setDataLoading(false);
+      } finally {
+        setDataLoading(false);
+      }
+    };
+
+    getAllRentProperties();
+  }, []);
 
   if (isLoading) return <Loading />;
   return (
@@ -53,107 +74,25 @@ export default function Rent() {
           <RentalReference
             rentalReferenceData={propertyReferenceDataWithoutUsageOption}
           />
-          {/* <div className='w-full flex lg:flex-row flex-col lg:w-[1154px] gap-[15px]'>
-            
-            <div className='flex flex-col gap-2 w-full'>
-              {found.isFound && (
-                <div className='flex justify-between'>
-                  {' '}
-                  <h2 className='text-[18px] leading-[28.8px] text-[#1976D2] font-semibold'>
-                    {found.count} match Found
-                  </h2>
-                  <h2 className='flex gap-[5px] lg:hidden'>
-                    <svg
-                      width='24'
-                      height='25'
-                      viewBox='0 0 24 25'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'>
-                      <path
-                        fillRule='evenodd'
-                        clipRule='evenodd'
-                        d='M7 4.5C6.73478 4.5 6.48043 4.60536 6.29289 4.79289C6.10536 4.98043 6 5.23478 6 5.5V19.5C6 19.7652 6.10536 20.0196 6.29289 20.2071C6.48043 20.3946 6.73478 20.5 7 20.5H17C17.2652 20.5 17.5196 20.3946 17.7071 20.2071C17.8946 20.0196 18 19.7652 18 19.5L18 9.91421L12.5859 4.50011L7 4.5ZM4.87868 3.37868C5.44129 2.81607 6.20435 2.5 7 2.5H12.586C13.1163 2.50011 13.6251 2.71086 14.0001 3.08589M14.0001 3.08589L19.414 8.49979C19.414 8.49975 19.414 8.49982 19.414 8.49979C19.789 8.87476 19.9999 9.38345 20 9.91379V19.5C20 20.2957 19.6839 21.0587 19.1213 21.6213C18.5587 22.1839 17.7957 22.5 17 22.5H7C6.20435 22.5 5.44129 22.1839 4.87868 21.6213C4.31607 21.0587 4 20.2957 4 19.5V5.5C4 4.70435 4.31607 3.94129 4.87868 3.37868M8 12.5C8 11.9477 8.44772 11.5 9 11.5H15C15.5523 11.5 16 11.9477 16 12.5C16 13.0523 15.5523 13.5 15 13.5H9C8.44772 13.5 8 13.0523 8 12.5ZM8 16.5C8 15.9477 8.44772 15.5 9 15.5H15C15.5523 15.5 16 15.9477 16 16.5C16 17.0523 15.5523 17.5 15 17.5H9C8.44772 17.5 8 17.0523 8 16.5Z'
-                        fill='#FF3D00'
-                      />
-                      <rect
-                        x='8'
-                        y='11.5'
-                        width='8'
-                        height='2'
-                        rx='1'
-                        fill='#FF3D00'
-                      />
-                      <rect
-                        x='8'
-                        y='15.5'
-                        width='8'
-                        height='2'
-                        rx='1'
-                        fill='#FF3D00'
-                      />
-                    </svg>
-                    <span
-                      onClick={viewSelectedBrief}
-                      className='text-base leading-[25.6px] font-medium text-[#FF3D00]'>
-                      {text}
-                    </span>
-                  </h2>
-                </div>
-              )}
-              <div
-                className={`${
-                  found.isFound ? 'lg:w-[858px]' : 'lg:w-[1154px]'
-                } w-full flex flex-col gap-[21px] lg:grid ${
-                  found.isFound ? 'lg:grid-cols-3' : 'lg:grid-cols-4'
-                } ${
-                  isSelectedBriefClicked ? 'hidden lg:grid' : 'flex lg:grid'
-                }`}>
-                {Array.from({ length: 8 }).map((__, idx: number) => (
-                  <Card key={idx} />
-                ))}
-              </div>
-            </div>
-           
-            {found.isFound && (
-              <div
-                className={`lg:flex flex-col lg:border-l-[1px] lg:border-[#A8ADB7] lg:pl-[20px] ${
-                  isSelectedBriefClicked ? 'flex lg:flex' : 'hidden lg:flex'
-                }`}>
-                <h2 className='text-[24px] leading-[38.4px] text-[#09391C] font-epilogue font-semibold'>
-                  Submit for inspection
-                </h2>
-                <div className='lg:w-[266px] w-full flex flex-col gap-[14px]'>
-                  {Array.from({ length: 2 }).map((__, idx: number) => (
-                    <Card isRed={true} key={idx} />
-                  ))}
-                </div>
-                <Button
-                  green={true}
-                  value='Submit'
-                  onClick={() => {
-                    setRentPage({ isSubmitForInspectionClicked: true });
-                  }}
-                  className='py-[12px] px-[24px] h-[64px] text-[#FFFFFF] text-base leading-[25.6px] font-bold mt-6'
-                />
-              </div>
-            )}
-          </div> */}
           <div className='w-full px-[20px] flex flex-col lg:w-[1153px]'>
-            <div className='flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-[20px]'>
-              {Array.from({ length: 8 }).map((__, idx: number) => (
-                <HouseFrame
-                  key={idx}
-                  title='Sample'
-                  image={imgSample}
-                  location=''
-                  bedroom={4}
-                  bathroom={3}
-                  carPark={2}
-                  onClick={() => {
-                    router.push(`/buy_page/details/${idx}`);
-                  }}
-                />
-              ))}
+            <div className='flex flex-col justify-center items-center md:grid md:grid-cols-2 lg:grid-cols-3 gap-[20px]'>
+              {isDataLoading ? (
+                <p>Failed to fetch data</p>
+              ) : (
+                data?.map((item: HouseFrameProps, idx: number) => (
+                  <HouseFrame
+                    key={idx}
+                    title={item.propertyType}
+                    images={item?.pictures}
+                    features={item.features}
+                    location={`${item.location.state}, ${item.location.area}, ${item.location.localGovernment}`}
+                    bedroom={item.noOfBedrooms}
+                    onClick={() => {
+                      router.push(`/buy_page/details/${item._id}`);
+                    }}
+                  />
+                ))
+              )}
             </div>
           </div>
         </div>
--- src/components/house-frame.tsx
+++ src/components/house-frame.tsx
@@ -1,21 +1,22 @@
 /** @format */
 
-import Image, { StaticImageData } from 'next/image';
+import Image from 'next/image';
 import React, { FC } from 'react';
 import locationIcon from '@/svgs/location.svg';
+import imgSample from '@/assets/assets.png';
 
 interface HouseFrameProps {
-  image: StaticImageData;
+  images: string[];
   title: string;
   location: string;
-  bedroom: number;
-  bathroom: number;
-  carPark: number;
+  bedroom?: number;
+  bathroom?: number;
+  carPark?: number;
+  features?: { featureName: string; id: string }[];
   onClick?: () => void;
 }
 
 const HouseFrame: FC<HouseFrameProps> = ({
-  image,
   title,
   location,
   bedroom,
@@ -28,7 +29,10 @@ const HouseFrame: FC<HouseFrameProps> = ({
       onClick={onClick}
       className='w-full min-h-[342px] flex flex-col gap-[11px] cursor-pointer'>
       <Image
-        src={image}
+        // src={
+        //   images?.length !== 0 && images !== undefined ? images[0] : imgSample
+        // }
+        src={imgSample}
         width={1000}
         height={1000}
         alt=''
@@ -53,14 +57,18 @@ const HouseFrame: FC<HouseFrameProps> = ({
             <span className='text-[14px] leading-[22px]'>Bedroom</span>
             <span className='text-[14px] leading-[22px]'>{bedroom}</span>
           </div>
-          <div className='flex flex-col border-r-[1px] pr-[10px] border-[#C7CAD0]'>
-            <span className='text-[14px] leading-[22px]'>Bathroom</span>
-            <span className='text-[14px] leading-[22px]'>{bathroom}</span>
-          </div>
-          <div className='flex flex-col'>
-            <span className='text-[14px] leading-[22px]'>Car Park</span>
-            <span className='text-[14px] leading-[22px]'>{carPark}</span>
-          </div>
+          {bathroom && (
+            <div className='flex flex-col border-r-[1px] pr-[10px] border-[#C7CAD0]'>
+              <span className='text-[14px] leading-[22px]'>Bathroom</span>
+              <span className='text-[14px] leading-[22px]'>{bathroom}</span>
+            </div>
+          )}
+          {carPark && (
+            <div className='flex flex-col'>
+              <span className='text-[14px] leading-[22px]'>Car Park</span>
+              <span className='text-[14px] leading-[22px]'>{carPark}</span>
+            </div>
+          )}
         </div>
       </div>
     </div>
