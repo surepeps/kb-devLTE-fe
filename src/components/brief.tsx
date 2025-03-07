15e06cfd834fc70e504ddd5aee74b733b8f33596
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2025-03-07 03:52:18 +0100

    `feat: Added user authentication and redirection logic, updated API calls and context providers`

--- src/components/brief.tsx
+++ src/components/brief.tsx
@@ -1,11 +1,14 @@
 /** @format */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { DataProps } from '@/types/agent_data_props';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import DetailsToCheck from '@/components/detailsToCheck';
 import { ShowTableProps } from '@/types/show_table';
 import ShowTable from '@/components/showTable';
 import Briefs from './mobileBrief';
+import axios from 'axios';
+import { URLS } from '@/utils/URLS';
 
 interface TotalBriefProps extends ShowTableProps {
   detailsToCheck: DataProps;
@@ -20,6 +23,27 @@ const Brief: FC<TotalBriefProps> = ({
   detailsToCheck,
   headerData,
 }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [briefsData, setBriefsData] = useState([]);
+
+  useEffect(() => {
+    const getTotalBriefs = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(URLS.BASE + '/agent/properties');
+        const data = response.data;
+        console.log(data);
+        setIsLoading(false);
+        setBriefsData(data);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getTotalBriefs();
+  }, []);
   return (
     <div className='lg:w-[863px] w-full mt-[60px] flex items-center justify-center'>
       {showFullDetails ? (
