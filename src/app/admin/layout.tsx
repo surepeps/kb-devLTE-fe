2d174b80596db8e03a2d9a00e80c4c6a6734d32b
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2025-03-22 19:03:00 +0100

    `Refactor admin layout and components for improved styling and functionality`

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,14 +1,14 @@
+/** @format */
 
-
-import { ReactNode } from "react";
-import AdminNavbar from "@/components/admincomponents/navbar";
+import { ReactNode } from 'react';
+import AdminNavbar from '@/components/admincomponents/navbar';
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="w-screen min-h-full flex ">
+    <main className='w-full min-h-screen flex'>
       <AdminNavbar />
       {/* Content */}
-      <section className="flex-1 p-4">{children}</section>
+      <section className='flex-1 p-4 lg:w-1/2'>{children}</section>
     </main>
   );
 }
