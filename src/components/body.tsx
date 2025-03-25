6ea8825abfac5c045e1bef53d7d6eb91268d1fca
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2025-03-25 13:03:10 +0100

    `feat: Added approve, reject, and delete briefs functionality`
    Former-commit-id: 67fba845478698c9e628990be608d780e986c616

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -4,6 +4,9 @@ import { usePageContext } from '@/context/page-context';
 import React, { Fragment, ReactNode } from 'react';
 import SubmitPopUp from './submit';
 import OnUpgradeNotification from './settings-components/on-ugrade-notification';
+import ApproveBriefs from './admincomponents/approveBriefs';
+import RejectBriefs from './admincomponents/rejectBriefs';
+import DeleteBriefs from './admincomponents/deleteBriefs';
 //import SubmitPopUp from './submit';
 
 const Body = ({ children }: { children: ReactNode }) => {
@@ -13,6 +16,7 @@ const Body = ({ children }: { children: ReactNode }) => {
     viewImage,
     isSubmittedSuccessfully,
     settings,
+    dashboard,
   } = usePageContext();
   return (
     <Fragment>
@@ -22,13 +26,19 @@ const Body = ({ children }: { children: ReactNode }) => {
             isModalOpened ||
             viewImage ||
             isSubmittedSuccessfully ||
-            settings.isUpgradeButtonClicked) &&
+            settings.isUpgradeButtonClicked ||
+            dashboard.approveBriefsTable.isApproveClicked ||
+            dashboard.approveBriefsTable.isDeleteClicked ||
+            dashboard.approveBriefsTable.isRejectClicked) &&
           'filter brightness-[30%] bg-[#EEF1F1] transition-all duration-500 overflow-hidden'
         } w-[100%]`}>
         {children}
       </section>
       {isSubmittedSuccessfully && <SubmitPopUp />}
       {settings.isUpgradeButtonClicked && <OnUpgradeNotification />}
+      {dashboard.approveBriefsTable.isApproveClicked && <ApproveBriefs />}
+      {dashboard.approveBriefsTable.isRejectClicked && <RejectBriefs />}
+      {dashboard.approveBriefsTable.isDeleteClicked && <DeleteBriefs />}
     </Fragment>
   );
 };
