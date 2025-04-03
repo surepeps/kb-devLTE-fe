771b34efd7b37a42f73ab2e8fd9d83d9f8d47e83
Abasay <abdulsalamasheem@gmail.com>
2025-04-03 18:22:13 +0100

    fix

--- src/components/section2.tsx
+++ src/components/section2.tsx
@@ -53,8 +53,8 @@ const Section2 = () => {
         }
 
         const data = await response.json();
-        setProperties(data);
-        setCardData(data);
+        setProperties(data.data);
+        setCardData(data.data);
       } catch (err: any) {
         if (err.name !== 'AbortError') {
           console.error(err);
@@ -73,17 +73,16 @@ const Section2 = () => {
   }, []);
 
   return (
-    <section className='flex justify-center items-center bg-[#8DDB901A] pb-[30px]'>
-      <div className='container min-h-[700px] flex flex-col justify-center items-center gap-[20px] px-[20px] overflow-hidden'>
-        <div className='min-h-[128px] w-full lg:w-[870px] flex flex-col justify-center items-center gap-[9px] pt-[40px]'>
-          <h2 className='text-[24px] leading-[28.13px] lg:text-[36px] lg:leading-[57.6px] md:leading-[32px] text-[#09391C] text-center font-semibold'>
+    <section className="flex justify-center items-center bg-[#8DDB901A] pb-[30px]">
+      <div className="container min-h-[700px] flex flex-col justify-center items-center gap-[20px] px-[20px] overflow-hidden">
+        <div className="min-h-[128px] w-full lg:w-[870px] flex flex-col justify-center items-center gap-[9px] pt-[40px]">
+          <h2 className="text-[24px] leading-[28.13px] lg:text-[36px] lg:leading-[57.6px] md:leading-[32px] text-[#09391C] text-center font-semibold">
             See What Other Buyers Are Exploring
           </h2>
-          <p className='text-[#5A5D63] text-[18px] leading-[28.8px] tracking-[5%] font-normal text-center'>
-            Your trusted partner in Lagos&apos; real estate market. Since 2020,
-            we&apos;ve been delivering expert solutions with integrity and
-            personalized service, helping you navigate property sales, rentals,
-            and more. Let us help you find your perfect property today
+          <p className="text-[#5A5D63] text-[18px] leading-[28.8px] tracking-[5%] font-normal text-center">
+            Your trusted partner in Lagos&apos; real estate market. Since 2020, we&apos;ve been delivering expert
+            solutions with integrity and personalized service, helping you navigate property sales, rentals, and more.
+            Let us help you find your perfect property today
           </p>
         </div>
 
@@ -91,9 +90,10 @@ const Section2 = () => {
           ref={buttonsRef}
           className={`w-[344px] md:min-w-[466px] min-h-[38px] py-[10px] gap-[15px] flex ${
             areButtonsVisible && 'slide-from-top'
-          }`}>
+          }`}
+        >
           <Button
-            value='Property insight'
+            value="Property insight"
             green={buttons.button1}
             onClick={() => {
               setButtons({
@@ -108,7 +108,7 @@ const Section2 = () => {
           />
           <Button
             green={buttons.button2}
-            value='Land for sale'
+            value="Land for sale"
             onClick={() => {
               setButtons({
                 button1: false,
@@ -122,7 +122,7 @@ const Section2 = () => {
           />
           <Button
             green={buttons.button3}
-            value='Rent'
+            value="Rent"
             onClick={() => {
               setButtons({
                 button1: false,
@@ -140,7 +140,8 @@ const Section2 = () => {
           initial={{ opacity: 0, x: 20 }}
           animate={areHousesVisible ? { opacity: 1, x: 0 } : {}}
           transition={{ duration: 0.3 }}
-          className={`lg:w-[1154px] w-full min-h-[446px] grid lg:grid-cols-4 lg:gap-[83px] grid-cols-1 md:grid-cols-2 gap-[24px]`}>
+          className={`lg:w-[1154px] w-full min-h-[446px] grid lg:grid-cols-4 lg:gap-[83px] grid-cols-1 md:grid-cols-2 gap-[24px]`}
+        >
           {/* {cardDataArray.map((item, idx: number) => {
             if (idx <= 3) {
               return (
@@ -160,20 +161,13 @@ const Section2 = () => {
                   onClick={() => {
                     addBrief(property);
                     // Retrieve existing selectedBriefs from localStorage
-                    const existingBriefs = JSON.parse(
-                      localStorage.getItem('selectedBriefs') || '[]'
-                    );
+                    const existingBriefs = JSON.parse(localStorage.getItem('selectedBriefs') || '[]');
 
                     // Ensure it's an array and add the new property
-                    const updatedBriefs = Array.isArray(existingBriefs)
-                      ? [...existingBriefs, property]
-                      : [property];
+                    const updatedBriefs = Array.isArray(existingBriefs) ? [...existingBriefs, property] : [property];
 
                     // Save back to localStorage
-                    localStorage.setItem(
-                      'selectedBriefs',
-                      JSON.stringify(updatedBriefs)
-                    );
+                    localStorage.setItem('selectedBriefs', JSON.stringify(updatedBriefs));
 
                     if (selectedBriefs.has(property)) {
                       return toast.success('Already added for inspection');
@@ -201,8 +195,7 @@ const Section2 = () => {
                     {
                       header: 'Documents',
                       value: `<ol class='' style='list-style: 'dics';'>${property.docOnProperty.map(
-                        (item: { _id: string; docName: string }) =>
-                          `<li key={${item._id}>${item.docName}</li>`
+                        (item: { _id: string; docName: string }) => `<li key={${item._id}>${item.docName}</li>`
                       )}<ol>`,
                     },
                   ]}
@@ -212,23 +205,16 @@ const Section2 = () => {
             }
           })}
         </motion.div>
-        <div className='flex justify-center items-center mt-6'>
+        <div className="flex justify-center items-center mt-6">
           <button
             onClick={() => {
               window.location.href = '/buy_page';
             }}
-            type='button'
-            className='flex justify-center items-center gap-2'>
-            <span className='text-base text-[#09391C] leading-[25px] font-semibold'>
-              Show more
-            </span>{' '}
-            <Image
-              src={arrowIcon}
-              width={12}
-              height={15}
-              alt=''
-              className='w-[12px] h-[15px]'
-            />
+            type="button"
+            className="flex justify-center items-center gap-2"
+          >
+            <span className="text-base text-[#09391C] leading-[25px] font-semibold">Show more</span>{' '}
+            <Image src={arrowIcon} width={12} height={15} alt="" className="w-[12px] h-[15px]" />
           </button>
         </div>
       </div>
