ea269078ef7222224cbc106a29ef53e5937f92d5
Abasay <abdulsalamasheem@gmail.com>
2025-04-03 16:10:46 +0100

    fix: eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    ' react-hooks/exhaustive-deps': 'warn',
+    // 'react-hooks/rules-of-hooks': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
 ];
 
 export default eslintConfig;
