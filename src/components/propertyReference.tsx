971c2fffe22f2214c0475ad26ae1b15a18153ab3
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2025-04-12 22:37:26 +0100

    refactoring create brief page to add briefs from the urgent request

--- src/components/propertyReference.tsx
+++ src/components/propertyReference.tsx
@@ -77,14 +77,13 @@ const PropertyReference = ({
         return;
       }
 
-
       const payload = {
         propertyType: values.propertyType,
         state: formik.values.selectedLGA.split(',')[0].trimStart(),
         localGovernment: values.selectedLGA, // assumption, no local govt input on the design
         area: 'N/A', // assumption, same
         minPrice: 0,
-        maxPrice: 1000000000,
+        maxPrice: 0,
         usageOptions: formik.values.usageOption,
         additionalFeatures: formik.values.desireFeatures,
         minBedrooms: 1,
@@ -141,7 +140,7 @@ const PropertyReference = ({
         localGovernment: formik.values.selectedLGA, //assumption, no local govt input on the design
         area: 'N/A', //assumption, same,
       },
-      price: 2048344930, //assumption, supposing we are using the budget range instead
+      price: 0,
       docOnProperty: propertyDoc,
       propertyFeatures: {
         additionalFeatures: formik.values.desireFeatures,
@@ -155,7 +154,10 @@ const PropertyReference = ({
     };
 
     console.log(payload);
-    setPropertyReference(payload);
+    setPropertyReference({
+      type: 'buy',
+      payload: payload,
+    });
     setRentPage({
       submitPreference: true,
       isSubmitForInspectionClicked: false,
@@ -350,37 +352,6 @@ const PropertyReference = ({
           formik={formik}
           submitPreference={submitReference}
         />
-        // <div className='min-h-[106px] w-full lg:w-[1153px] py-[23px] px-[20px] md:px-[40px] gap-[10px] bg-white'>
-        //   <div className='w-full min-h-[60px] flex md:flex-row flex-col gap-[20px] justify-between md:gap-0'>
-        //     <div className='flex flex-col'>
-        //       <h2 className='text-[18px] text-[#09391C] leading-[28.8px] font-medium'>
-        //         Can&apos;t find the brief you&apos;re looking for? Don&apos;t
-        //         worry! We&apos;ll provide a reference brief for you
-        //       </h2>
-        //       <div className='flex gap-[5px] flex-wrap'>
-        //         {Object.entries(formik.values).map(([, items], idx: number) => {
-        //           if (typeof items === 'object') {
-        //             return (
-        //               <Crumb
-        //                 key={idx}
-        //                 text={Object.values(items)
-        //                   .map((item) => item)
-        //                   .join(', ')}
-        //               />
-        //             );
-        //           }
-        //           return <Crumb key={idx} text={items} />;
-        //         })}
-        //       </div>
-        //     </div>
-        //     <button
-        //       type='button'
-        //       onClick={submitReference}
-        //       className='text-base leading-[25.6px] font-bold text-[#09391C] lg:min-w-[245px] h-[58px] border-[1px] py-[12px] px-[24px] border-[#09391C]'>
-        //       Submit your preferences
-        //     </button>
-        //   </div>
-        // </div>
       )}
     </Fragment>
   );
@@ -460,20 +431,6 @@ const Select: React.FC<SelectProps> = ({
     </label>
   );
 };
-
-// const Crumb = ({ text }: { text: any }) => {
-//   return (
-//     <Fragment>
-//       {text ? (
-//         <div
-//           dangerouslySetInnerHTML={{ __html: text }}
-//           className='bg-[#F7F7F8] min-h-[28px] min-w-fit py-[3px] px-[6px] text-[14px] text-[#0B0D0C] leading-[22.4px] font-normal tracking-[0.1px] cursor-not-allowed font-ubuntu rounded-[5px] hover:bg-[#e1e1e1] transition-all duration-300'
-//         />
-//       ) : null}
-//     </Fragment>
-//   );
-// };
-
 /**
  * Component Menu List
  */
