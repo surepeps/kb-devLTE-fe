d348dac31cb41103dcf30763eb94500bd7c20a97
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2025-04-24 20:10:15 +0100

    `feat: updated download image functionality and view image component`

--- src/components/downloadImage.tsx
+++ src/components/downloadImage.tsx
@@ -1,30 +1,19 @@
 /** @format */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
+import toast from 'react-hot-toast';
 
-const DownloadImage = ({
-  downloadImage,
-  status,
-}: {
-  downloadImage: () => void;
-  status?: 'idle' | 'pending' | 'success' | 'failed';
-}) => {
+const DownloadImage = ({ downloadImage }: { downloadImage: () => void }) => {
   return (
     <motion.button
-      onClick={downloadImage}
+      onClick={() => {
+        downloadImage();
+        toast.success('Image downloaded successfully!');
+      }}
       type='button'
       className='w-fit px-2 py-1 rounded-[5px] bg-gray-100 hover:bg-gray-300 transition duration-500 border-[2px] border-gray-200 shadow-md mt-[10px]'>
-      {status === 'pending' && (
-        <span className='text-gray-500'>Downloading...</span>
-      )}
-      {status === 'success' && (
-        <span className='text-green-500'>Downloaded!</span>
-      )}
-      {status === 'failed' && (
-        <span className='text-red-500'>Failed to download</span>
-      )}
-      {status === 'idle' && <span className='text-gray-500'>Download</span>}
+      Download
     </motion.button>
   );
 };
