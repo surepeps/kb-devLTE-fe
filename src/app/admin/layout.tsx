27313f15665fbae5952b961b30ed5ef2efdb0a65
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2025-04-30 22:58:54 +0100

    committing new changes - brief management, working on the create house brief

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,6 +5,7 @@ import AdminNavbar from '@/components/admincomponents/navbar';
 import { usePathname, useRouter } from 'next/navigation';
 import Cookies from 'js-cookie';
 import { EditBriefProvider } from '@/context/admin-context/brief-management/edit-brief';
+import { CreateBriefProvider } from '@/context/admin-context/brief-management/create-brief';
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
@@ -19,11 +20,15 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
   }, [isLoginPage, router]);
 
   return (
-    <EditBriefProvider>
-      <main className='w-full flex'>
-        {!isLoginPage && <AdminNavbar />}
-        <section className='md:flex-1 w-full p-4 lg:w-1/2'>{children}</section>
-      </main>
-    </EditBriefProvider>
+    <CreateBriefProvider>
+      <EditBriefProvider>
+        <main className='w-full flex'>
+          {!isLoginPage && <AdminNavbar />}
+          <section className='md:flex-1 w-full p-4 lg:w-1/2'>
+            {children}
+          </section>
+        </main>
+      </EditBriefProvider>
+    </CreateBriefProvider>
   );
 }
