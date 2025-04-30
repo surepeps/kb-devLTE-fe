cf82e892a7239ba662a02fe5d6cb8d91eff14bde
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2025-04-30 22:58:54 +0100

    committing new changes - brief management, working on the create house brief
    Former-commit-id: 27313f15665fbae5952b961b30ed5ef2efdb0a65

--- src/app/admin/brief_management/page.tsx
+++ src/app/admin/brief_management/page.tsx
@@ -1,5 +1,7 @@
+/** @format */
+
 'use client';
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, Fragment } from 'react';
 import {
   faMagnifyingGlass,
   faQuestion,
@@ -11,6 +13,7 @@ import Select from 'react-select';
 import Loading from '@/components/loading';
 import { useFormik } from 'formik';
 import BriefLists from '@/components/admincomponents/brief_lists';
+import CreateBrief from '@/components/admincomponents/createBrief';
 
 const boxData: BoxNotificationProps[] = [
   {
@@ -38,6 +41,8 @@ const boxData: BoxNotificationProps[] = [
 export default function BriefManagement() {
   const isLoading = useLoading();
   const [data, setData] = useState<BoxNotificationProps[]>(boxData);
+  const [isCreateBriefModalOpened, setIsCreateBriefModalOpened] =
+    useState<boolean>(false);
 
   // Memoize the updateBriefTotals function to prevent unnecessary re-renders
   const updateBriefTotals = useCallback((totals: Record<string, number>) => {
@@ -72,81 +77,98 @@ export default function BriefManagement() {
   return isLoading ? (
     <Loading />
   ) : (
-    <section className='flex flex-col w-full md:w-[initial]'>
-      {/* Search & Help Button */}
-      <div className='flex justify-between items-center'>
-        <div className='md:w-3/5 mt-2 h-12 flex relative items-center'>
-          <FontAwesomeIcon
-            icon={faMagnifyingGlass}
-            size='lg'
-            width={24}
-            height={24}
-            className='text-[#A7A9AD] absolute left-3 w-[24px] h-[24px]'
-          />
-          <input
-            type='text'
-            placeholder='Search for Agent, Briefs'
-            className='w-full h-full pl-12 border border-gray-300 bg-transparent rounded-md'
-          />
-        </div>
-        <button
-          type='button'
-          className='bg-black w-[30px] h-[30px] flex items-center justify-center rounded-full shadow-md'>
-          {''}
-          <FontAwesomeIcon
-            icon={faQuestion}
-            color='#fff'
-            size='sm'
-            className='bg-[#000] rounded-full shadow-md'
-          />
-        </button>
-      </div>
-
-      {/* Dashboard Header */}
-      <div className='flex md:flex-row flex-col justify-between mt-6 md:mr-6 w-full md:w-[initial] gap-2 md:gap-0'>
-        <div className='flex flex-col gap-1 md:gap-0'>
-          <h2
-            className={`text-3xl font-bold text-[#2E2C34] ${archivo.className}`}>
-            Briefs Management
-          </h2>
-          <p className={`text-sm text-[#84818A] ${archivo.className}`}>
-            Showing your Account metrics from{' '}
-            <span>31st Mar, 2025 to {currentDate}</span>
-          </p>
-        </div>
-        <div className='flex h-[48px] w-fit md:w-[initial] items-center bg-white px-4 rounded-lg'>
-          <div className='text-[#84818A] flex items-center text-sm'>
-            Show stats:
-            <Select
-              className='text-[#2E2C34] text-sm ml-1'
-              styles={{
-                control: (styles) => ({
-                  ...styles,
-                  border: 'none',
-                  boxShadow: 'none',
-                  cursor: 'pointer',
-                  outline: 'none',
-                }),
-              }}
-              options={statsOptions}
-              defaultValue={statsOptions}
-              value={formik.values.selectedStat}
-              onChange={(options) => {
-                formik.setFieldValue('selectedStat', options);
-              }}
+    <Fragment>
+      <section className='flex flex-col w-full md:w-[initial]'>
+        {/* Search & Help Button */}
+        <div className='flex justify-between items-center'>
+          <div className='md:w-3/5 mt-2 h-12 flex relative items-center'>
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              size='lg'
+              width={24}
+              height={24}
+              className='text-[#A7A9AD] absolute left-3 w-[24px] h-[24px]'
+            />
+            <input
+              type='text'
+              placeholder='Search for Agent, Briefs'
+              className='w-full h-full pl-12 border border-gray-300 bg-transparent rounded-md'
             />
           </div>
+          <button
+            type='button'
+            className='bg-black w-[30px] h-[30px] flex items-center justify-center rounded-full shadow-md'>
+            {''}
+            <FontAwesomeIcon
+              icon={faQuestion}
+              color='#fff'
+              size='sm'
+              className='bg-[#000] rounded-full shadow-md'
+            />
+          </button>
         </div>
-      </div>
-      <div className='flex overflow-x-auto hide-scrollbar gap-[30px] w-full mt-6'>
-        {data.map((item: BoxNotificationProps, index: number) => (
-          <BoxNotification key={index} {...item} />
-        ))}
-      </div>
-      <div className='w-full'>
-        <BriefLists setBriefTotals={updateBriefTotals} />
-      </div>
-    </section>
+
+        {/* Dashboard Header */}
+        <div className='flex md:flex-row flex-col justify-between mt-6 md:mr-6 w-full md:w-[initial] gap-2 md:gap-0'>
+          <div className='flex flex-col gap-1 md:gap-0'>
+            <h2
+              className={`text-3xl font-bold text-[#2E2C34] ${archivo.className}`}>
+              Briefs Management
+            </h2>
+            <p className={`text-sm text-[#84818A] ${archivo.className}`}>
+              Showing your Account metrics from{' '}
+              <span>31st Mar, 2025 to {currentDate}</span>
+            </p>
+          </div>
+          <div className='flex gap-[20px]'>
+            {/**Button */}
+            <button
+              onClick={() =>
+                setIsCreateBriefModalOpened(!isCreateBriefModalOpened)
+              }
+              className={`w-[189px] gap-[19px] flex justify-center items-center bg-[#8DDB90] hover:bg-[#2a542b] transition-all duration-200 h-[50px] text-base font-medium ${archivo.className} text-white`}
+              type='button'
+              title='Create brief'>
+              Create brief
+            </button>
+            <div className='flex h-[48px] w-fit md:w-[initial] items-center bg-white px-4 rounded-lg'>
+              <div className='text-[#84818A] flex items-center text-sm'>
+                Show stats:
+                <Select
+                  className='text-[#2E2C34] text-sm ml-1'
+                  styles={{
+                    control: (styles) => ({
+                      ...styles,
+                      border: 'none',
+                      boxShadow: 'none',
+                      cursor: 'pointer',
+                      outline: 'none',
+                    }),
+                  }}
+                  options={statsOptions}
+                  defaultValue={statsOptions}
+                  value={formik.values.selectedStat}
+                  onChange={(options) => {
+                    formik.setFieldValue('selectedStat', options);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='flex overflow-x-auto hide-scrollbar gap-[30px] w-full mt-6'>
+          {data.map((item: BoxNotificationProps, index: number) => (
+            <BoxNotification key={index} {...item} />
+          ))}
+        </div>
+        <div className='w-full'>
+          <BriefLists setBriefTotals={updateBriefTotals} />
+        </div>
+      </section>
+      {isCreateBriefModalOpened && (
+        <CreateBrief closeModal={setIsCreateBriefModalOpened} />
+      )}
+    </Fragment>
   );
 }
 
