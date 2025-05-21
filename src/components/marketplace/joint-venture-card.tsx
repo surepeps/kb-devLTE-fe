73ab1b5caa4a5bd89c6ea720fa82c077f5bed1ab
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2025-05-21 14:58:54 +0100

    `Added isComingFromSubmitLol state and props to various components for marketplace feature`

--- src/components/marketplace/joint-venture-card.tsx
+++ src/components/marketplace/joint-venture-card.tsx
@@ -19,15 +19,23 @@ interface CardDataProps {
   // style?: React.CSSProperties;
   // isDisabled?: boolean;
 
-    isRed?: boolean;
-    cardData: { header: string; value: string }[];
-    onClick?: () => void;
-    className?: string;
-    images: StaticImport[];
-    isPremium?: boolean;
-    style?: React.CSSProperties;
-    isDisabled?: boolean;
-    onCardPageClick?: () => void;
+  isRed?: boolean;
+  cardData: { header: string; value: string }[];
+  onClick?: () => void;
+  className?: string;
+  images: StaticImport[];
+  isPremium?: boolean;
+  style?: React.CSSProperties;
+  isDisabled?: boolean;
+  onCardPageClick?: () => void;
+  /**
+   * coming from submit Lol button
+   */
+  isComingFromSubmitLol: boolean;
+  setIsComingFromSubmitLol: (type: boolean) => void;
+  setPropertySelected: (type: any[]) => void;
+  property: any;
+  setIsAddInspectionModalOpened: (type: boolean) => void;
 }
 
 const JointVentureModalCard = ({
@@ -38,11 +46,15 @@ const JointVentureModalCard = ({
   images = [],
   style,
   isDisabled,
-  onCardPageClick
+  onCardPageClick,
+  isComingFromSubmitLol,
+  setIsComingFromSubmitLol,
+  setPropertySelected,
+  property,
+  setIsAddInspectionModalOpened,
 }: CardDataProps) => {
   const { setViewImage, setImageData } = usePageContext();
   const cardRef = useRef<HTMLDivElement | null>(null);
-
 
   return (
     <Fragment>
@@ -54,78 +66,82 @@ const JointVentureModalCard = ({
         exit={{ opacity: 0, y: 20 }}
         viewport={{ once: true }}
         ref={cardRef}
-        style={style}
-      >
+        style={style}>
         <div className='md:w-[261px] w-full p-[12px] flex flex-col gap-[11px] bg-[#FFFFFF] on'>
           <div className='min-h-[62px] w-full flex gap-[10px] items-end relative'>
-        <Image
-          src={images[0] ?? imageSample}
-          width={61}
-          height={62}
-          onClick={() => {
-            setImageData(images);
-            setViewImage(true);
-          }}
-          className='w-[61px] h-[62px] object-cover'
-          alt=''
-        />
-        <h3 className='font-semibold text-[#000000] text-lg'>
-          N {Number(300000000).toLocaleString()}
-        </h3>
-        {/**Premium */}
-        <div className=' w-[98px] top-0 right-0 md:right-auto absolute mb-[35px] ml-[151px] h-[28px] py-[8px] px-[6px] text-white flex justify-between items-center bg-[#FF3D00]'>
-          <span>Premium</span>
-          <FontAwesomeIcon icon={faStarOfDavid} size='sm' />
-        </div>
-      </div>
-      <div className='h-[81px] w-full flex flex-col gap-[5px]'>
-        <div className='flex gap-[5px] items-center'>
-          <Image
-            src={markerIcon}
-            alt='marker'
-            width={16}
-            height={16}
-            className='w-[16px] h-[16px]'
-          />
-          <span className='text-xs text-[#000000]'>GRA, Ikeja, Lagos.</span>
-        </div>
-        <div className='flex flex-wrap gap-[2%] min-h-[57px]'>
-          {/**Land sqft */}
-          <div className='bg-[#E4EFE7] px-[7px] h-[26px] w-[59%] text-xs text-[#000000] flex items-center mb-2'>
-            4000 sqft
+            <Image
+              src={images[0] ?? imageSample}
+              width={61}
+              height={62}
+              onClick={() => {
+                setImageData(images);
+                setViewImage(true);
+              }}
+              className='w-[61px] h-[62px] object-cover'
+              alt=''
+            />
+            <h3 className='font-semibold text-[#000000] text-lg'>
+              N {Number(300000000).toLocaleString()}
+            </h3>
+            {/**Premium */}
+            <div className=' w-[98px] top-0 right-0 md:right-auto absolute mb-[35px] ml-[151px] h-[28px] py-[8px] px-[6px] text-white flex justify-between items-center bg-[#FF3D00]'>
+              <span>Premium</span>
+              <FontAwesomeIcon icon={faStarOfDavid} size='sm' />
+            </div>
           </div>
-          <div className='bg-[#E4EFE7] px-[7px] text-xs text-[#000000] h-[26px]  w-[39%] flex items-center'>
-            Fence
+          <div className='h-[81px] w-full flex flex-col gap-[5px]'>
+            <div className='flex gap-[5px] items-center'>
+              <Image
+                src={markerIcon}
+                alt='marker'
+                width={16}
+                height={16}
+                className='w-[16px] h-[16px]'
+              />
+              <span className='text-xs text-[#000000]'>GRA, Ikeja, Lagos.</span>
+            </div>
+            <div className='flex flex-wrap gap-[2%] min-h-[57px]'>
+              {/**Land sqft */}
+              <div className='bg-[#E4EFE7] px-[7px] h-[26px] w-[59%] text-xs text-[#000000] flex items-center mb-2'>
+                4000 sqft
+              </div>
+              <div className='bg-[#E4EFE7] px-[7px] text-xs text-[#000000] h-[26px]  w-[39%] flex items-center'>
+                Fence
+              </div>
+              <div className='bg-[#E4EFE7] px-[7px] text-xs text-[#000000] h-[26px] w-[100%] flex items-center min-w-fit'>
+                deed of assignment
+              </div>
+            </div>
           </div>
-          <div className='bg-[#E4EFE7] px-[7px] text-xs text-[#000000] h-[26px] w-[100%] flex items-center min-w-fit'>
-            deed of assignment
+          <hr />
+          <div className='flex flex-col gap-[10px]'>
+            <Button
+              value={`Submit LOI`}
+              onClick={() => {
+                setIsAddInspectionModalOpened(true);
+                setPropertySelected([property]);
+                setIsComingFromSubmitLol?.(true);
+              }}
+              type='button'
+              // green={isRed ? false : true}
+              //red={isRed}
+              //onClick={onClick}
+              className='min-h-[38px] px-[24px] bg-[#1976D2] text-[#FFFFFF] text-base leading-[25.6px] font-bold'
+            />
+            <Button
+              value={`Select for Inspection`}
+              type='button'
+              //green={isRed ? false : true}
+              //red={isRed}
+              onClick={onClick}
+              isDisabled={isDisabled} // Disable the button if the property is already selected
+              className={`min-h-[50px] py-[12px] px-[24px] ${
+                isDisabled
+                  ? 'bg-gray-400 cursor-not-allowed'
+                  : 'bg-[#8DDB90] hover:bg-[#76c77a]'
+              } text-[#FFFFFF] text-base leading-[25.6px] font-bold`}
+            />
           </div>
-        </div>
-      </div>
-      <hr />
-      <div className='flex flex-col gap-[10px]'>
-        <Button
-          value={`Submit LOI`}
-          type='button'
-          // green={isRed ? false : true}
-          //red={isRed}
-          //onClick={onClick}
-          className='min-h-[38px] px-[24px] bg-[#1976D2] text-[#FFFFFF] text-base leading-[25.6px] font-bold'
-        />
-        <Button
-          value={`Select for Inspection`}
-          type='button'
-          //green={isRed ? false : true}
-          //red={isRed}
-          onClick={onClick}
-          isDisabled={isDisabled} // Disable the button if the property is already selected
-          className={`min-h-[50px] py-[12px] px-[24px] ${
-            isDisabled
-              ? 'bg-gray-400 cursor-not-allowed'
-              : 'bg-[#8DDB90] hover:bg-[#76c77a]'
-          } text-[#FFFFFF] text-base leading-[25.6px] font-bold`}
-        />
-      </div>
         </div>
       </motion.div>
     </Fragment>
