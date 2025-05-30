0b89e49f1e660fc895aad82d4983345efe269ab2
Oluwafemi Omolounnu <oluwafemiomolounnu@gmail.com>
2025-05-30 10:11:18 +0100

    working on seller inspection flow
    Former-commit-id: 102e5354932605034453be6f2e53fb9c150a74a3

--- src/app/seller-loi/[clientID]/page.tsx
+++ src/app/seller-loi/[clientID]/page.tsx
@@ -1,18 +1,18 @@
-/** @format */
-'use client';
-import Index from '@/components/seller-lol';
-import { useParams, useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+// /** @format */
+// 'use client';
+// import Index from '@/components/seller-lol';
+// import { useParams, useSearchParams } from 'next/navigation';
+// import React, { useEffect, useState } from 'react';
 
-const Page = () => {
-  //const searchParams = useSearchParams();
-  const [ID, setID] = useState<string>('');
-  const { id } = useParams();
+// const Page = () => {
+//   //const searchParams = useSearchParams();
+//   const [ID, setID] = useState<string>('');
+//   const { id } = useParams();
 
-  useEffect(() => {
-    if (typeof id === 'string') return setID(id);
-  }, []);
-  return <Index potentialClientID={ID} />;
-};
+//   useEffect(() => {
+//     if (typeof id === 'string') return setID(id);
+//   }, []);
+//   return <Index potentialClientID={ID} />;
+// };
 
-export default Page;
+// export default Page;
