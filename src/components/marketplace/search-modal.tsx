0811cad3b5c2ca3fe6ffeccca2f5f73286339665
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2025-05-30 18:03:55 +0100

    `Refactor marketplace filter and search functionality`

--- src/components/marketplace/search-modal.tsx
+++ src/components/marketplace/search-modal.tsx
@@ -675,6 +675,7 @@ const SearchModal = ({
             setPropertiesSelected(Array.from(selectedBriefsList));
             setIsAddInspectionModalOpened(true);
           }}
+          handleSearch={handleSearch}
         />
       ) : (
         <>{userSelectedMarketPlace && renderDynamicComponent()}</>
