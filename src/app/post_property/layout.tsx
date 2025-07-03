2a3ca07e67bf0684e4915f466d2abc67a4dc4260
surepeps <gatukurh1@gmail.com>
2025-07-03 11:50:06 +0000

    Update post property layout to include context provider
    cgen-a2a8a95c2870400fb7135aa41407fd67

--- src/app/post_property/layout.tsx
+++ src/app/post_property/layout.tsx
@@ -1,12 +1,13 @@
 /** @format */
 
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
+import { PostPropertyProvider } from "@/context/post-property-context";
 
 export const metadata: Metadata = {
-  title: 'Khabiteq | Post Property',
+  title: "Khabiteq | Post Property",
   description: `Simplifying real estate transactions in Lagos. Buy, sell, rent, and manage properties with ease through Khabi-Teq's trusted platform`,
   icons: {
-    icon: '/khabi-teq.svg',
+    icon: "/khabi-teq.svg",
   },
 };
 
@@ -15,5 +16,5 @@ export default function SellPageLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return <PostPropertyProvider>{children}</PostPropertyProvider>;
 }
