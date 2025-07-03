e7fffea1a955b99925727cdd5d3668d1407a33e9
surepeps <gatukurh1@gmail.com>
2025-07-03 11:53:54 +0000

    Update agent page to redirect to dashboard when authenticated
    cgen-0c39b39c7dd442ad893137b710d82ab6

--- src/app/agent/page.tsx
+++ src/app/agent/page.tsx
@@ -1,23 +1,23 @@
 /** @format */
 
-'use client';
-import React, { Fragment, useEffect } from 'react';
-import HeroSection from '@/components/homepage/hero';
-import Loading from '@/components/loading-component/loading';
-import Section1 from '@/components/section1';
-import Section3 from '@/components/homepage/section3';
-import FAQs from '@/components/homepage/FAQs';
-import HelpButton from '@/components/general-components/helpButton';
-import { usePageContext } from '@/context/page-context';
-import { useLoading } from '@/hooks/useLoading';
-import agentImage from '@/assets/Agentpic.png';
-import { useRouter } from 'next/navigation';
-import { useUserContext } from '@/context/user-context';
-import Image from 'next/image';
-import { epilogue } from '@/styles/font';
-import image from '@/assets/Rectangle.png';
-import { motion } from 'framer-motion';
-import Link from 'next/link';
+"use client";
+import React, { Fragment, useEffect } from "react";
+import HeroSection from "@/components/homepage/hero";
+import Loading from "@/components/loading-component/loading";
+import Section1 from "@/components/section1";
+import Section3 from "@/components/homepage/section3";
+import FAQs from "@/components/homepage/FAQs";
+import HelpButton from "@/components/general-components/helpButton";
+import { usePageContext } from "@/context/page-context";
+import { useLoading } from "@/hooks/useLoading";
+import agentImage from "@/assets/Agentpic.png";
+import { useRouter } from "next/navigation";
+import { useUserContext } from "@/context/user-context";
+import Image from "next/image";
+import { epilogue } from "@/styles/font";
+import image from "@/assets/Rectangle.png";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Agent = () => {
   //Simulating the loading page
@@ -30,12 +30,16 @@ const Agent = () => {
   useEffect(() => {
     if (!user) {
       return;
-    } else if (user.userType !== 'Agent') {
-      router.push('/agent');
-    } else if (!user.accountApproved) {
-      router.push('/agent/under-review');
+    } else if (user.userType === "Agent") {
+      if (!user.accountApproved) {
+        router.push("/agent/under-review");
+      } else {
+        router.push("/agent/dashboard");
+      }
+    } else if (user.userType === "Landowners") {
+      router.push("/landlord");
     } else {
-      router.push('/agent/briefs');
+      router.push("/");
     }
   }, [user, router]);
 
@@ -46,21 +50,22 @@ const Agent = () => {
       <section
         className={`w-full  ${
           (isContactUsClicked || isModalOpened) &&
-          'filter brightness-[30%] bg-[#EEF1F1] transition-all duration-500 overflow-hidden'
-        }`}>
-        <main className='w-full bg-[#EEF1F1]'>
+          "filter brightness-[30%] bg-[#EEF1F1] transition-all duration-500 overflow-hidden"
+        }`}
+      >
+        <main className="w-full bg-[#EEF1F1]">
           <HeroSection
             image={agentImage}
-            headingText='Partner with'
+            headingText="Partner with"
             description='"Partner with Khabi-Teq for exclusive listings, referral commissions, and seamless transactions to grow your real estate business'
-            buttonText1='Partner with us'
-            buttonText2='Login'
-            bgColor='#0A3E72'
-            borderColor='#63A3BE'
-            onButton1Click={() => router.push('/auth?from=agent')}
+            buttonText1="Partner with us"
+            buttonText2="Login"
+            bgColor="#0A3E72"
+            borderColor="#63A3BE"
+            onButton1Click={() => router.push("/auth?from=agent")}
           />
           <Section1
-            headingColor='#000000'
+            headingColor="#000000"
             displayCounts={false}
             text={`Join our trusted network of agents and access exclusive property listings, connect with verified buyers and sellers, and grow your real estate business seamlessly`}
           />
@@ -109,10 +114,10 @@ const Agent = () => {
           <Section3
             isAgentPage={true}
             isHomepage={false}
-            heading='Benefit of partnering with Khabi-teq'
-            headingColor='#000000'
+            heading="Benefit of partnering with Khabi-teq"
+            headingColor="#000000"
           />
-          <FAQs isHomePage={false} headingColor='#000000' />
+          <FAQs isHomePage={false} headingColor="#000000" />
           <HelpButton isHomePage={false} />
         </main>
       </section>
