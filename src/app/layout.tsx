623208a1210dbc2130183ff47f0930bb509de6ad
surepeps <gatukurh1@gmail.com>
2025-07-07 22:31:23 +0000

    Add notification provider to layout
    cgen-44d8f7d75965466aae2e21649bbc58b3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ import { ModalProvider } from "@/context/modalContext";
 import { CreateBriefProvider } from "@/context/create-brief-context";
 import { SelectedBriefsProvider } from "@/context/selected-briefs-context";
 import { MarketplaceProvider } from "@/context/marketplace-context";
+import { NotificationProvider } from "@/context/notification-context";
 import Homepage from "@/app/homepage/page";
 import Countdown from "./coming-soon-modal/page";
 // import { GoogleAnalytics } from '@next/third-parties/google';
