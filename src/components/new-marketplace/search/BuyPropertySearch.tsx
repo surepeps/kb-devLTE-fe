d4cdaa2f4e4b26f5b8e1e29549b41cf915180dec
surepeps <gatukurh1@gmail.com>
2025-07-07 23:45:46 +0000

    Improve filter state management and initialization
    cgen-48c275621eba49a5b7006a0be8ff3e79

--- src/components/new-marketplace/search/BuyPropertySearch.tsx
+++ src/components/new-marketplace/search/BuyPropertySearch.tsx
@@ -138,13 +138,15 @@ const BuyPropertySearch = () => {
     },
   });
 
-  // Filter states
+  // Filter states with better initialization
   const [filters, setFilters] = useState({
     selectedState: "",
     selectedLGA: "",
     selectedArea: "",
+    locationDisplay: "",
     priceRange: { min: 0, max: 0 },
     documentTypes: [] as string[],
+    usageOptions: [] as string[],
     bedrooms: undefined as number | undefined,
     bathrooms: undefined as number | undefined,
     landSize: {
