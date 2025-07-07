ae2cbd3c07f0bac6c926a02587f915ac6dca3903
surepeps <gatukurh1@gmail.com>
2025-07-07 11:56:47 +0000

    Add missing useEffect import to LOIUploadModal
    cgen-32792400510b4099b6be0f5bb7f0ed95

--- src/components/new-marketplace/modals/LOIUploadModal.tsx
+++ src/components/new-marketplace/modals/LOIUploadModal.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
 "use client";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
