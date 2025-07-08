61c6fb4d08c77b0a713657b985cb6614721f69bd
surepeps <gatukurh1@gmail.com>
2025-07-08 00:37:58 +0000

    Update DocumentTypeFilter to use dynamic data
    cgen-58b1619fd53744969f52ccb2e3ad4c74

--- src/components/new-marketplace/filters/DocumentTypeFilter.tsx
+++ src/components/new-marketplace/filters/DocumentTypeFilter.tsx
@@ -5,6 +5,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { X, Check } from "lucide-react";
 import useClickOutside from "@/hooks/clickOutside";
+import { getDocumentTypes } from "@/data/filter-data";
 
 interface DocumentTypeFilterProps {
   isOpen: boolean;
