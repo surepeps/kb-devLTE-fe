74ef7f0ac3fa81ad3c5a11c30a855137be806151
surepeps <gatukurh1@gmail.com>
2025-07-08 09:28:44 +0000

    Update clearInspectionSelection method
    cgen-3dddb55624b445da91637f0ea9a3d0b3

--- src/context/new-marketplace-context.tsx
+++ src/context/new-marketplace-context.tsx
@@ -404,7 +404,7 @@ export const NewMarketplaceProvider: React.FC<{
   );
 
   const clearInspectionSelection = useCallback(
-    (tab: "buy" | "jv" | "rent") => {
+    (tab: "buy" | "jv" | "rent" | "shortlet") => {
       updateTabState(tab, (state) => ({
         ...state,
         selectedForInspection: [],
