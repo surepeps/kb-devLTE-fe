80acc0b3b8aba4ebfb1276e089978a9c0e885013
surepeps <gatukurh1@gmail.com>
2025-07-08 09:34:00 +0000

    Add ShortletTab import and update renderTabContent
    cgen-3b861161b4d14c368c9d8b7998dab0b8

--- src/components/new-marketplace/index.tsx
+++ src/components/new-marketplace/index.tsx
@@ -8,6 +8,7 @@ import MarketplaceTabs from "./MarketplaceTabs";
 import BuyTab from "./tabs/BuyTab";
 import JointVentureTab from "./tabs/JointVentureTab";
 import RentTab from "./tabs/RentTab";
+import ShortletTab from "./tabs/ShortletTab";
 import AddForInspection from "./AddForInspection";
 import StandardPreloader from "./StandardPreloader";
 
