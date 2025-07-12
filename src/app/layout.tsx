13992a4eb47a3c0590b4755eea0f7957765fc0fe
surepeps <gatukurh1@gmail.com>
2025-07-12 22:03:03 +0000

    Remove props from Homepage component in layout.tsx
    cgen-7402c35e829d41368838d91ec2fc675b

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,10 +48,10 @@ export default function RootLayout({
                     <body
                       className={`${roboto.variable} ${archivo.variable} ${epilogue.variable} ${ubuntu.variable} antialiased`}
                     >
-                      {" "} 
+                      {" "}
                       {/*This was refactored to accomodate Admin routes without the Header and Footer  ||Gb */}
                       <HeaderFooterWrapper isComingSoon={SHOW_COMING_SOON}>
-                        <Homepage isComingSoon={SHOW_COMING_SOON} />
+                        <Homepage />
                         <Countdown />
                       </HeaderFooterWrapper>
                       <Toaster />
