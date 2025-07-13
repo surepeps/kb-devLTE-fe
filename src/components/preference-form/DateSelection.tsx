036b142f11bdc1db1f6807aad3f79a2d768685a5
surepeps <gatukurh1@gmail.com>
2025-07-13 05:22:43 +0000

    Remove updateFormData from useEffect dependency array in DateSelection
    cgen-d8cb494e59674ea89638e4249d16c544

--- src/components/preference-form/DateSelection.tsx
+++ src/components/preference-form/DateSelection.tsx
@@ -58,7 +58,7 @@ const DateSelection: React.FC<DateSelectionProps> = ({ className = "" }) => {
         checkOutDate,
       },
     });
-  }, [checkInDate, checkOutDate, updateFormData]);
+  }, [checkInDate, checkOutDate]);
 
   // Get minimum date (today)
   const getMinDate = useCallback(() => {
