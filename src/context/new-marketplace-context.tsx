add82393a7c14eef9ee8c5488a8ae0377b48a5ae
surepeps <gatukurh1@gmail.com>
2025-07-22 10:17:07 +0000

    Remove debug logging from updateTabState
    cgen-bfaac4b171fb4c02800dfe410cec181a

--- src/context/new-marketplace-context.tsx
+++ src/context/new-marketplace-context.tsx
@@ -275,21 +275,9 @@ export const NewMarketplaceProvider: React.FC<{
       tab: "buy" | "jv" | "rent" | "shortlet",
       updater: (state: TabState) => TabState,
     ) => {
-      console.log(`updateTabState called for ${tab} tab`);
       switch (tab) {
         case "buy":
-          setBuyTab((prevState) => {
-            const newState = updater(prevState);
-            console.log(`BuyTab state updated:`, {
-              prevProperties: prevState.properties.length,
-              newProperties: newState.properties.length,
-              prevStatus: prevState.formikStatus,
-              newStatus: newState.formikStatus,
-              prevSearchStatus: prevState.searchStatus.status,
-              newSearchStatus: newState.searchStatus.status
-            });
-            return newState;
-          });
+          setBuyTab(updater);
           break;
         case "jv":
           setJvTab(updater);
