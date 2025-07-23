b504783aa44d0c2532129d35e1d7d1e0ad2fff1f
surepeps <gatukurh1@gmail.com>
2025-07-23 05:27:55 +0000

    Remove backdrop click to close for LOI upload modal
    cgen-f001b122a8304db38408f98919557102
    
    
    Former-commit-id: ea24e51d1ca33b96ebb2ed9a82380baa91119639

--- src/components/modals/SimplifiedLOIUploadModal.tsx
+++ src/components/modals/SimplifiedLOIUploadModal.tsx
@@ -196,7 +196,6 @@ const SimplifiedLOIUploadModal: React.FC<SimplifiedLOIUploadModalProps> = ({
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4 overflow-hidden"
-          onClick={onClose}
         >
           <motion.div
             initial={{ scale: 0.95, opacity: 0 }}
