55b3451c8f2a4cc9b5d8fe1948e9b2c0d804ad9e
surepeps <gatukurh1@gmail.com>
2025-07-23 13:48:07 +0000

    Fix PropertyData type casting in post-property page
    cgen-70dafc58f3f94716a28eef0d98a07762

--- src/app/post-property/page.tsx
+++ src/app/post-property/page.tsx
@@ -610,7 +610,7 @@ const PostProperty = () => {
           {/* Main Content with Formik */}
           <Formik
             initialValues={propertyData}
-            validationSchema={getValidationSchema(currentStep, propertyData as Record<string, unknown>)}
+            validationSchema={getValidationSchema(currentStep, propertyData as unknown as Record<string, unknown>)}
             onSubmit={() => {}}
             enableReinitialize
           >
