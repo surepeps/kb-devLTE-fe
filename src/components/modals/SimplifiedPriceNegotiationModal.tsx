b088fdfc64eb5ac427259abfde28ea727146ffe3
surepeps <gatukurh1@gmail.com>
2025-07-23 05:27:39 +0000

    Remove backdrop click to close for price negotiation modal
    cgen-97b7352de94e4eaf947dc020b865584b

--- src/components/modals/SimplifiedPriceNegotiationModal.tsx
+++ src/components/modals/SimplifiedPriceNegotiationModal.tsx
@@ -173,7 +173,6 @@ const SimplifiedPriceNegotiationModal: React.FC<SimplifiedPriceNegotiationModalP
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4 overflow-hidden"
-          onClick={onClose}
         >
           <motion.div
             initial={{ scale: 0.95, opacity: 0 }}
