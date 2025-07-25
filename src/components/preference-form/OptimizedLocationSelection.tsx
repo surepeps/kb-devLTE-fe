f07c7d022871c08a27155d5a73f04c72b72e2301
surepeps <gatukurh1@gmail.com>
2025-07-25 04:44:21 +0000

    Enhance select styles to prevent dancing
    cgen-aaea12bfa2674ab0ac2426ca4a5d017c

--- src/components/preference-form/OptimizedLocationSelection.tsx
+++ src/components/preference-form/OptimizedLocationSelection.tsx
@@ -113,10 +113,17 @@ const customSelectStyles = {
     boxShadow:
       "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
     zIndex: 50,
+    position: "absolute",
+    width: "100%",
   }),
   menuList: (provided: any) => ({
     ...provided,
     padding: "4px",
+    maxHeight: "200px",
+  }),
+  menuPortal: (provided: any) => ({
+    ...provided,
+    zIndex: 9999,
   }),
 };
 
