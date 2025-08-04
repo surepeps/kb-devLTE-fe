96fc0a6199332489e19dd18868019942e0163c69
surepeps <gatukurh1@gmail.com>
2025-08-04 14:11:33 +0000

    Create outright sales post property page
    cgen-eb6290b9c2014ddfb352fd111b3baed1
    
    
    Former-commit-id: 57869750460a92bf338436e01ee591fd074443ef

---
+++ src/app/post-property/outright-sales/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import SharedPostPropertyForm from "@/components/post-property-components/SharedPostPropertyForm";
+
+const OutrightSalesPage = () => {
+  return (
+    <SharedPostPropertyForm
+      propertyType="sell"
+      pageTitle="List Your Property - Outright Sales"
+      pageDescription="Follow these simple steps to list your property for outright sales and connect with potential buyers"
+    />
+  );
+};
+
+export default OutrightSalesPage;
