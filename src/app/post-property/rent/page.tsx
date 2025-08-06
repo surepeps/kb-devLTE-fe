d1075fffe913e8378a056fa55c10ba1f8dcdbcd6
surepeps <gatukurh1@gmail.com>
2025-08-06 12:56:36 +0000

    Update rent page to use RentPropertyForm
    cgen-8b80cbc1608f46c38fa5b7810e7d57c6

--- src/app/post-property/rent/page.tsx
+++ src/app/post-property/rent/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import React from "react";
-import SharedPostPropertyForm from "@/components/post-property-components/SharedPostPropertyForm";
+import RentPropertyForm from "@/components/post-property-components/forms/RentPropertyForm";
 
 const RentPage = () => {
   return (
-    <SharedPostPropertyForm
-      propertyType="rent"
+    <RentPropertyForm
       pageTitle="List Your Property - For Rent"
       pageDescription="Follow these simple steps to list your property for rent and connect with potential tenants"
     />
