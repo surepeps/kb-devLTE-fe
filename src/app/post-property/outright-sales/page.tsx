7511fa9c3b601e26f375e4aac7dbeaa1c37423e9
surepeps <gatukurh1@gmail.com>
2025-08-06 12:56:55 +0000

    Update outright sales page to use OutrightSalesPropertyForm
    cgen-06191e2a394e4494983bb7e87a87c7ee

--- src/app/post-property/outright-sales/page.tsx
+++ src/app/post-property/outright-sales/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import React from "react";
-import SharedPostPropertyForm from "@/components/post-property-components/SharedPostPropertyForm";
+import OutrightSalesPropertyForm from "@/components/post-property-components/forms/OutrightSalesPropertyForm";
 
 const OutrightSalesPage = () => {
   return (
-    <SharedPostPropertyForm
-      propertyType="sell"
+    <OutrightSalesPropertyForm
       pageTitle="List Your Property - Outright Sales"
       pageDescription="Follow these simple steps to list your property for outright sales and connect with potential buyers"
     />
