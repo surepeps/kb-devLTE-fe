c941d85a99e43afda09df1e199936faa6ac8677c
surepeps <gatukurh1@gmail.com>
2025-08-06 12:59:45 +0000

    Update outright sales update page to use UpdateOutrightSalesPropertyForm
    cgen-d382dbfbeae64d75a570f1848eccd633

--- src/app/update-property/[propertyId]/outright-sales/page.tsx
+++ src/app/update-property/[propertyId]/outright-sales/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import React from "react";
-import SharedUpdatePropertyForm from "@/components/post-property-components/SharedUpdatePropertyForm";
+import UpdateOutrightSalesPropertyForm from "@/components/post-property-components/forms/update/UpdateOutrightSalesPropertyForm";
 
 const UpdateOutrightSalesPage = () => {
   return (
-    <SharedUpdatePropertyForm
-      propertyType="sell"
+    <UpdateOutrightSalesPropertyForm
       pageTitle="Update Property - Outright Sales"
       pageDescription="Update your property information to keep your listing current and attractive to potential buyers"
     />
