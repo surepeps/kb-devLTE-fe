d1206d76b742cab7f91b677ecb62aa542ac099e2
surepeps <gatukurh1@gmail.com>
2025-08-06 12:56:43 +0000

    Update joint venture page to use JointVenturePropertyForm
    cgen-d6636ca83aa14b8a87645e478226e820
    
    
    Former-commit-id: 10833e447ddebcdd1d7cc2f636dfa6a4673698c1

--- src/app/post-property/joint-venture/page.tsx
+++ src/app/post-property/joint-venture/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import React from "react";
-import SharedPostPropertyForm from "@/components/post-property-components/SharedPostPropertyForm";
+import JointVenturePropertyForm from "@/components/post-property-components/forms/JointVenturePropertyForm";
 
 const JointVenturePage = () => {
   return (
-    <SharedPostPropertyForm
-      propertyType="jv"
+    <JointVenturePropertyForm
       pageTitle="List Your Property - Joint Venture"
       pageDescription="Follow these simple steps to list your property for joint venture partnerships and connect with potential investors"
     />
