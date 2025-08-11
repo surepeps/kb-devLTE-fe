ca4f2d9ea917354a0c54fecc54468b807e9df138
surepeps <gatukurh1@gmail.com>
2025-08-11 12:52:25 +0000

    Add Toaster component to layout
    cgen-b68fe78a1d7f4428a853f90162029c5b
    
    
    Former-commit-id: fe3e862c249755ceff0a2a4b00625a4415fa7f3b

--- src/app/third-party-verification/layout.tsx
+++ src/app/third-party-verification/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 
 export default function ThirdPartyVerificationLayout({ children }: { children: React.ReactNode }) {
   return (
