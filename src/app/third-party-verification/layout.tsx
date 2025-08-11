d765a351b4bf47b93abed7de1bc138c86872d9be
surepeps <gatukurh1@gmail.com>
2025-08-11 12:52:39 +0000

    Import global CSS styles
    cgen-4c1f5874912047d086f88ca6f2aed128
    
    
    Former-commit-id: 7d954b04fd1811bc67dd099df4302009ff2f1c7e

--- src/app/third-party-verification/layout.tsx
+++ src/app/third-party-verification/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
+import '../globals.css';
 
 export default function ThirdPartyVerificationLayout({ children }: { children: React.ReactNode }) {
   return (
