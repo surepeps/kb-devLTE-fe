1e44ce09278c1e14ce769e4d20d227025f309132
surepeps <gatukurh1@gmail.com>
2025-08-12 16:05:46 +0000

    Create test page for document iframe preview
    cgen-6dbc3d733a054be7b290e5bada5616b1
    
    
    Former-commit-id: ea360ff0c94c27058ee6dc354c21fca212fe9e78

---
+++ src/app/test-document-preview/page.tsx
@@ -0,0 +1,9 @@
+import DocumentPreviewDemo from '@/components/document-verification/DocumentPreviewDemo';
+
+export default function TestDocumentPreviewPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8 px-4">
+      <DocumentPreviewDemo />
+    </div>
+  );
+}
