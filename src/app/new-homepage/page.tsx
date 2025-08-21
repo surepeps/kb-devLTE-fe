a76d0f5e3ff203558279d5aff1cdda839b7222b8
surepeps <gatukurh1@gmail.com>
2025-08-21 10:07:33 +0000

    Enable enhanced header in new homepage
    cgen-b841cde9e0f64eccb077e3bb25542349
    
    
    Former-commit-id: 571daa095c12312204da471be7b749d11025ab2f

--- src/app/new-homepage/page.tsx
+++ src/app/new-homepage/page.tsx
@@ -54,7 +54,7 @@ const NewHomepage = ({
   return (
     <Fragment>
       {/* Enhanced Header */}
-      {/* <EnhancedHeader isComingSoon={isComingSoon} /> */}
+      <EnhancedHeader isComingSoon={isComingSoon} />
 
       <section className={`w-full filter ${isComingSoon && "blur-sm"}`}>
         <main className="w-full bg-[#FFFEFB]">
