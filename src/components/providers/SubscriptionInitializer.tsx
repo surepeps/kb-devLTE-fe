4ef66d0966636f2f932f5db87560527dac244a29
surepeps <gatukurh1@gmail.com>
2025-09-18 12:13:51 +0000

    SubscriptionInitializer to sync user profile to Redux and fetch catalog
    cgen-abc66ea6b1b948d88bf74b4370fcdced
    
    
    Former-commit-id: 7476757a372e7cc794d0bfe2b5349b08d287e090

---
+++ src/components/providers/SubscriptionInitializer.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useEffect } from 'react';
+import { useAppDispatch } from '@/store';
+import { fetchFeaturesCatalog, initializeFromProfile } from '@/store/subscriptionFeaturesSlice';
+import { useUserContext } from '@/context/user-context';
+
+export default function SubscriptionInitializer() {
+  const dispatch = useAppDispatch();
+  const { user, isInitialized } = useUserContext();
+
+  useEffect(() => {
+    dispatch(fetchFeaturesCatalog());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!isInitialized) return;
+    dispatch(initializeFromProfile(user as any));
+  }, [dispatch, user, isInitialized]);
+
+  return null;
+}
