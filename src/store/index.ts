d5c9d0dc66ad758e66825a0614fd690adae34ec8
surepeps <gatukurh1@gmail.com>
2025-09-18 12:37:01 +0000

    Remove client hooks from store index
    cgen-71b71bcdb6c840baab959b58ac56009c

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
 import subscriptionFeaturesReducer from './subscriptionFeaturesSlice';
 
 export const store = configureStore({
@@ -11,6 +10,3 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
