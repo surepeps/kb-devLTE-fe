587bd765d3f7d03b7fac1e1701d4840b50df231b
surepeps <gatukurh1@gmail.com>
2025-09-18 13:24:27 +0000

    Mark SubscriptionInitializer as client
    cgen-37016bf921824a5b82442b69bb0b91ab

--- src/components/providers/SubscriptionInitializer.tsx
+++ src/components/providers/SubscriptionInitializer.tsx
@@ -1,4 +1,5 @@
 "use client";
+"use client";
 import { useEffect } from 'react';
 import { useAppDispatch } from '@/store/hooks';
 import { fetchFeaturesCatalog, initializeFromProfile } from '@/store/subscriptionFeaturesSlice';
