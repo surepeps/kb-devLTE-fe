39af35ecd573fa70330c50400498a6ed40bf7465
surepeps <gatukurh1@gmail.com>
2025-09-22 06:47:35 +0000

    Make promo-context a client component
    cgen-9e5f8a921dc349bdb67d19760a296454

--- src/context/promo-context.tsx
+++ src/context/promo-context.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+"use client";
+
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { PROMOTIONS, Promotion } from "@/data/promotions";
 import { usePathname } from "next/navigation";
