2ca5778773f36e60ab57f109bf3f8da2efb8b723
surepeps <gatukurh1@gmail.com>
2025-09-30 15:20:54 +0000

    Gate Joint Venture page with LISTINGS
    cgen-285fbd27dac246d99e32ac965fb8485c

--- src/app/post-property/joint-venture/page.tsx
+++ src/app/post-property/joint-venture/page.tsx
@@ -2,13 +2,17 @@
 
 import React from "react";
 import JointVenturePropertyForm from "@/components/post-property-components/forms/JointVenturePropertyForm";
+import FeatureGate from "@/components/access/FeatureGate";
+import { FEATURE_KEYS } from "@/hooks/useFeatureGate";
 
 const JointVenturePage = () => {
   return (
-    <JointVenturePropertyForm
-      pageTitle="List Your Property - Joint Venture"
-      pageDescription="Follow these simple steps to list your property for joint venture partnerships and connect with potential investors"
-    />
+    <FeatureGate featureKeys={[FEATURE_KEYS.LISTINGS]}>
+      <JointVenturePropertyForm
+        pageTitle="List Your Property - Joint Venture"
+        pageDescription="Follow these simple steps to list your property for joint venture partnerships and connect with potential investors"
+      />
+    </FeatureGate>
   );
 };
 
